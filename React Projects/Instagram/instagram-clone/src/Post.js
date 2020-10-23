1758b7f3286546a74223eda5dc5b3211f2d4088a
吴亚晨 <wuyachen@wuyachendeMacBook-Pro.local>
2020-10-22 20:55:49 -0400

    second commit

---
+++ React Projects/Instagram/instagram-clone/src/Post.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            <h2>Username</h2>
+
+            <img src=""/>
+            
+            <h4>Username: caption</h4>
+        </div>
+    )
+}
+
+export default Post
